15900d0c5d8fb59c9fc938b947333d4d4de1ad36
Muhammad-Asif-Ansari <muhammadasifansari101@gmail.com>
2024-09-26 23:22:32 +0500

    Assignment JavaScript
    Assignment JavaScript

---
+++ app.js
@@ -0,0 +1,142 @@
+// ASSIGNMENT 03:
+// Chapter 9-11;
+// QUESTION 01:
+// var cityName = prompt("Enter Your City Name");
+// if(cityName === "karachi"){
+// document.write("Welcome to the city of lights");
+// }
+
+// QUESTION 02:
+// var gender = prompt("Enter Your Gender");
+// if(gender === "male"){
+//     document.write("Good Morning Sir");
+// }
+// else if(gender === "female"){
+//     document.write("Good Morning Ma'am");
+// }
+// else{
+//     document.write("Invalid input");
+// }
+
+//  QUESTION 03:
+// var signalColor = prompt("Please enter the traffic signal color (red/yellow/green):");
+// if (signalColor === "red") {
+//     document.write("Must Stop");
+// } else if (signalColor === "yellow") {
+//     document.write("Ready to Move");
+// } else if (signalColor === "green") {
+//     document.write("Move Now");
+// } else {
+//     document.write("Invalid input. Please enter 'red', 'yellow', or 'green'.");
+// }
+
+//  QUESTION 04:
+// var fuel = prompt("Remaining fuel in car  (in liters):");
+// if (fuel < 0.25){
+//     document.write("Please refill the fuel in your car");
+// };
+
+// QUESTION 05:
+// (a).
+// var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true"); // Correct Code
+// }
+// // (b).
+// var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is false"); // Wrong Code
+
+// }
+// // (c).
+// var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is false");
+// }
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){
+// alert("condition 3 is false");
+// }
+// if(c === 14){
+// alert("condition 4 is true");
+// }
+// // (d).
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals"); //  Correct Code
+
+// }
+// // (e).
+// if (true){
+// alert("True");
+// }
+// if (false){
+// alert("False"); // False Not Count
+// }
+// // (f).
+// if("car" < "cat"){
+// alert("car is smaller than cat");  // Correct Code
+
+// }
+
+//  QUESTION 06:
+//  var num = prompt("Enter a Number");
+// if(num % 2 === 0){
+//     document.write("This is Even Number");
+// }
+// else{
+//     document.write("This is Odd Number");
+// }
+
+    
+//  QUESTION 07:
+// var num = prompt("Enter a Number");
+// if(num % 3 === 0){
+//     document.write("Number is divisible by 3");
+// }
+// else{
+//     document.write("Number is not divisible by 3");
+// }
+
+//  QUESTION 08:
+// var temperature = prompt("Enter a temperature");
+// if ( temperature > 40){
+//     alert("It is too hot outside");
+// }
+// else if ( temperature > 30){
+//     alert("The weather today is  Normal");
+
+// }
+// else if ( temperature > 20){
+//     alert("Today’s Weather is cool");
+// }
+// else if ( temperature > 10){
+//     alert("OMG! Today’s weather is so Cool");
+// }
+// else{
+//     alert("This temperature is not match");
+// }
+
+// QUESTION 09:
+// var num1 = prompt("Enter first Number");
+// var num2 = prompt("Enter Second Number");
+// var oper = prompt("Enter Operator(+, -, *, /, %) Number");
+// if (oper === "+"){
+//     num1 = num1 + num2
+// }else if (oper === "-"){
+//     num1 = num1 - num2
+// }else if (oper === "*"){
+//     num1 = num1 * num2
+// }else if (oper === "/"){
+//     num1 = num1 / num2
+// }else if (oper === "%"){
+//     num1 = num1 % num2
+// }
+// alert(num1);
+
+
+
